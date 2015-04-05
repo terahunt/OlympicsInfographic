a179980beb6f16f069ce784df2185972d33ffba4
Tera Hunt <terahunt101@gmail.com>
2015-04-05 00:11:47 -0400

    FIx slider bug and change layout.

---
+++ js/yearScript.js
@@ -0,0 +1,90 @@
+function getYear(n)
+		{
+			switch(n)
+			{
+				case "0":
+					return "1896"
+					break; 
+				case "1":
+					return "1900"
+					break;
+				case "2":
+					return "1904"
+					break;
+				case "3":
+					return "1908"
+					break;
+				case "4":
+					return "1912"
+					break;
+				case "5":
+					return "1920"
+					break;
+				case "6":
+					return "1924"
+					break;
+				case "7":
+					return "1928"
+					break;
+				case "8":
+					return "1932"
+					break;
+				case "9":
+					return "1936"
+					break;
+				case "10":
+					return "1948"
+					break;
+				case "11":
+					return "1952"
+					break;
+				case "12":
+					return "1956"
+					break;
+				case "13":
+					return "1960"
+					break;
+				case "14":
+					return "1964"
+					break;
+				case "15":
+					return "1968"
+					break;
+				case "16":
+					return "1972"
+					break;
+				case "17":
+					return "1976"
+					break;
+				case "18":
+					return "1980"
+					break;
+				case "19":
+					return "1984"
+					break;
+				case "20":
+					return "1988"
+					break;
+				case "21":
+					return "1992"
+					break;
+				case "22":
+					return "1996"
+					break;
+				case "23":
+					return "2000"
+					break;
+				case "24":
+					return "2004"
+					break;
+				case "25":
+					return "2008"
+					break;
+				case "26":
+					return "2012"
+					break; 
+				default: 
+					return "undefined"; 
+			}
+			
+		}
